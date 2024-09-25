4b435667e10897f2a5d312dbb76b506010db2e05
Niaev <trutnv@protonmail.com>
2024-09-25 17:50:47 -0300

    Fixed names from user to owner

--- routes/views.py
+++ routes/views.py
@@ -21,21 +21,21 @@ def login_page():
     
     return None
 
-@app.get('/app/users')
-def show_users_page():
-    """Display all users page"""
+@app.get('/app/owners')
+def show_owners_page():
+    """Display all owner page"""
 
     return None
 
-@app.get('/app/user/create')
-def create_user_page():
-    """Display user creation form"""
+@app.get('/app/owner/create')
+def create_owner_page():
+    """Display owner creation form"""
 
     return None
 
-@app.get('/app/user/update/<int:id>')
-def update_user_page(id:int):
-    """Display user creation form"""
+@app.get('/app/owner/update/<int:id>')
+def update_owner_page(id:int):
+    """Display owner creation form"""
 
     return None
 
