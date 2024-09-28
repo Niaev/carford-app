6c49e019ae5b7f1e7e06cb2fe276930116946edd
Niaev <trutnv@protonmail.com>
2024-09-28 08:07:01 -0300

    Working owners view

---
+++ static/js/get_owners.js
@@ -0,0 +1,47 @@
+const div = document.querySelector('#owners');
+const table = div.querySelector('.table');
+const tbody = table.querySelector('tbody');
+
+document.addEventListener('DOMContentLoaded', async () => {
+
+    let res;
+    try {
+        res = await fetch('/owners', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    } catch {
+        alert('An error happened');
+        return;
+    }
+
+    const json = await res.json();
+
+    if (res.status != 200) {
+        alert(await json.message);
+        return;
+    }
+
+    const ownersList = await json.owners;
+    ownersList.forEach(owner => {
+        tbody.innerHTML += `
+        <tr>
+            <td>${owner.id}</td>
+            <td>${owner.name}</td>
+            <td>${owner.email}</td>
+            <td>${owner.phone}</td>
+            <td>${owner.number_of_cars}</td>
+            <td>${owner.created_at}</td>
+            <td>
+                <a 
+                href="/app/owner/update/${owner.id}"
+                class="btn btn-primary" 
+                id="update-owner">Update</a>
+            </td>
+        </tr>
+        `;
+    });
+    
+});
